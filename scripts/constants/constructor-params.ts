d024ba08793257ccc290c307addffeba5b71ccf9
Ismail Can Vardar <ismailcanvardar@gmail.com>
2023-05-27 15:33:58 +0300

    update constructor parameters

--- scripts/constants/constructor-params.ts
+++ scripts/constants/constructor-params.ts
@@ -5,18 +5,12 @@ interface IConstructorParams {
     MTC: {
         totalSupply: BigNumber;
     },
-    RoyaltyFeeManager: {
-        MAXIMUM_FEE_PERCENTAGE: number;
-    };
 }
 
 const CONSTRUCTOR_PARAMS: IConstructorParams = {
     MTC: {
         totalSupply: toWei(String(10_000_000_000)),
     },
-    RoyaltyFeeManager: {
-        MAXIMUM_FEE_PERCENTAGE: 20
-    }
 };
 
 export default CONSTRUCTOR_PARAMS;

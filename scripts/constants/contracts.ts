0dd39b4074fb6784c6f77d6dbc24bbb97e01d261
Ismail Can Vardar <ismailcanvardar@gmail.com>
2023-05-27 15:32:55 +0300

    update contract namings

--- scripts/constants/contracts.ts
+++ scripts/constants/contracts.ts
@@ -3,17 +3,14 @@ interface IContracts {
   core: {
     Distributor: string;
     LiquidityPool: string;
-    MetatimeToken: string;
+    MTC: string;
     PrivateSaleTokenDistributor: string;
     StrategicPool: string;
     TokenDistributor: string;
-    MTC: string;
   };
   utils: {
-    DistributorProxyManager: string;
-    InitializedProxy: string;
     MultiSigWallet: string;
-    TokenDistributorProxyManager: string;
+    PoolFactory: string;
   };
 }
 
@@ -21,17 +18,14 @@ const CONTRACTS: IContracts = {
   core: {
     Distributor: "Distributor",
     LiquidityPool: "LiquidityPool",
-    MetatimeToken: "MetatimeToken",
+    MTC: "MTC",
     PrivateSaleTokenDistributor: "PrivateSaleTokenDistributor",
     StrategicPool: "StrategicPool",
     TokenDistributor: "TokenDistributor",
-    MTC: "MTC",
   },
   utils: {
-    DistributorProxyManager: "DistributorProxyManager",
-    InitializedProxy: "InitializedProxy",
     MultiSigWallet: "MultiSigWallet",
-    TokenDistributorProxyManager: "TokenDistributorProxyManager"
+    PoolFactory: "PoolFactory"
   }
 };
 

1f8a75333fedd095206c216423d28a17b08b5727
Ismail Can Vardar <ismailcanvardar@gmail.com>
2023-09-04 15:55:11 +0300

    alter deployment script

--- scripts/deploy/00_mtc_and_pools.ts
+++ scripts/deploy/00_mtc_and_pools.ts
@@ -11,51 +11,45 @@ const func: DeployFunction = async ({
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    const mtc = await deploy(CONTRACTS.core.MTC, {
-        from: deployer,
-        args: [CONSTRUCTOR_PARAMS.MTC.totalSupply],
-        log: true,
-        skipIfAlreadyDeployed: true,
-    });
+    try {
+        const mtc = await deploy(CONTRACTS.core.MTC, {
+            from: deployer,
+            args: [CONSTRUCTOR_PARAMS.MTC.totalSupply],
+            log: true,
+            skipIfAlreadyDeployed: true,
+        });
 
-    console.log("MTC deployed at", mtc.address);
+        console.log("MTC deployed at", mtc.address);
 
-    // const liquidityPool = await deploy(CONTRACTS.core.LiquidityPool, {
-    //     from: deployer,
-    //     args: [mtc.address],
-    //     log: true,
-    //     skipIfAlreadyDeployed: true,
-    // });
+        const liquidityPool = await deploy(CONTRACTS.core.LiquidityPool, {
+            from: deployer,
+            args: [mtc.address],
+            log: true,
+            skipIfAlreadyDeployed: true,
+        });
 
-    // console.log("Liquidity pool deployed at", liquidityPool.address);
+        console.log("Liquidity pool deployed at", liquidityPool.address);
 
-    // const strategicPool = await deploy(CONTRACTS.core.StrategicPool, {
-    //     from: deployer,
-    //     args: [mtc.address],
-    //     log: true,
-    //     skipIfAlreadyDeployed: true,
-    // });
+        const strategicPool = await deploy(CONTRACTS.core.StrategicPool, {
+            from: deployer,
+            args: [mtc.address],
+            log: true,
+            skipIfAlreadyDeployed: true,
+        });
 
-    // console.log("Strategic pool deployed at", strategicPool.address);
+        console.log("Strategic pool deployed at", strategicPool.address);
 
-    // const poolFactory = await deploy(CONTRACTS.utils.PoolFactory, {
-    //     from: deployer,
-    //     args: [],
-    //     log: true,
-    //     skipIfAlreadyDeployed: true,
-    // });
+        const poolFactory = await deploy(CONTRACTS.utils.PoolFactory, {
+            from: deployer,
+            args: [],
+            log: true,
+            skipIfAlreadyDeployed: true,
+        });
 
-    // console.log("Pool factory deployed at", poolFactory.address);
-
-    // // Pools that is derived from Distributor contract
-    // const distributors = filterObject(POOL_PARAMS, ([k, v]) => v.baseContract === BaseContract.Distributor);
-
-    // const distributorKeys = Object.keys(distributors);
-
-    // // Pools that is derived from TokenDistributor contract
-    // const tokenDistributors = filterObject(POOL_PARAMS, ([k, v]) => v.baseContract === BaseContract.TokenDistributor);
-
-    // const tokenDistributorKeys = Object.keys(tokenDistributors);
+        console.log("Pool factory deployed at", poolFactory.address);
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 export default func;
